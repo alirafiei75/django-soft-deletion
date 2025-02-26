ad50eae18c67355dc16111e73f8038fd30b6aa40
ali rafiei <103249712+alirafiei75@users.noreply.github.com>
2025-02-26 16:20:40 +0330

    update setup.py name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="django-soft-deletion",
+    name="django-soft-deletion-model",
     version="1.0.0",
     packages=find_packages(),
     install_requires=["Django==4.2"],
